88914ab6fb7ac48a9efd2ace5ca362fa5590c318
millent <html@thisummer.top>
2016-07-10 09:53:50 +0800

    Update cool.pc.js

--- pc/cool.pc.js
+++ pc/cool.pc.js
@@ -1,134 +1,125 @@
-(function(root){
-
-	function Factory(node){
-		this.node=node;
-	};
-	Factory.prototype.mousedown=function(fn){
-		this.on('touchstart',function(event){
-			if(event.targetTouches.length == 1){
-				fn(event.targetTouches[0]);
-			}
-		})
-	};
-	Factory.prototype.mouseup=function(fn){
-		this.on('touchend',function(event){
-			fn(event.targetTouches[0]);
-		})
-	};
-	Factory.prototype.mousemove=function(fn){
-		this.on('touchmove',function(event){
-			if(event.targetTouches.length == 1){
-				
-				fn(event.targetTouches[0]);
-			}
-		})
-	};
-	Factory.prototype.scroll=function(fn,fn2){
-		var root=this;
-		this.mousedown(function(e){
-			root.on('scroll',function(e){
-				fn(e);
-			})
-			if(fn2){
-				var first=document.body.scrollTop || document.documentElement.scrollTop;
-				root.mouseup(function(e){
-					
-					var last=first-document.body.scrollTop || document.documentElement.scrollTop;
-					if(last>0){
-						fn2(e,1);
-					}else if(last==0){
-						fn2(e,0);
-					}else{
-						fn2(e,-1);
-					}
-					
-					root.off(root.node,'touchend');
-					root.off(root.node,'scroll')
-
-				})	
-			}
-			
-		})
-	};
-	Factory.prototype.on=function(type,fn){
-		this.node.lib=this.node.lib||{};
-		this.node.lib[type]=this.node.lib[type]||[];
-		var g=function(event){
-			fn(event)
-		}
-		this.node.lib[type].push(g)
-
-		this.node.addEventListener(type,g,false)
-	};
-	Factory.prototype.off=function(node,type){
-		for(var i=0;i<node.lib[type].length;i++){
-			node.removeEventListener(type,node.lib[type][i],false);
-		}
-		node.lib[type]=[];
-	};
-	Factory.prototype.css=function(attr,value) {
-	    switch (arguments.length) {
-	        case 1:
-	            if (typeof arguments[0] == "object") { 
-	                for (var i in attr) this.node.style[i] = attr[i]
-	                return this;  
-	            }
-	            else { 
-	                return getComputedStyle(this.node, null)[attr]
-	            }
-	            break;
-	        case 2:
-	            this.node.style[attr] = value;
-	        	return this;    
-	            break;
-	        default:
-	            return "";
-	    }
-
-	};
-	Factory.prototype.hasClass=function(name,type){
-	    var arr=this.node.className.split(' ');
-	    var isFind=false;
-	    for(var i =0;i<arr.length;i++){
-	        if(arr[i]==name){
-	            isFind=true;
-	        }
-	    }
-	    if(type){
-	        return [isFind,arr];
-	    }else{
-	        return isFind;
-	    }
-	};
-	Factory.prototype.addClass=function(cls){
-	    if(!this.hasClass(cls)){
-	        this.node.className+=(" "+cls);
-	    }
-	    return this;
-	};
-	Factory.prototype.removeClass=function(name){
-	    if(this.hasClass(name,true)[0]){
-	        var arr=this.hasClass(name,true)[1];
-	        for(var i=0;i<arr.length;i++){
-	            if(arr[i]==name){
-	                arr.splice(i,1);
-	            }
-	        }
-	        this.node.className=arr.join(' ');
-	    }
-	    return this;
-	};
-	root.$=function(node){
-		if(typeof node=='object'){
-			return new Factory(node)
-		}else if(typeof node == 'function'){
-			if (document.addEventListener) {
+;(function(window){
+	var q={
+		ready:function(callback) {
+		    if (document.addEventListener) {
 		        document.addEventListener('DOMContentLoaded', function () {
 		            document.removeEventListener('DOMContentLoaded', arguments.callee, false);
-		            node();
+		            callback();
 		        }, false)
 		    }
+		    else if (document.attachEvent) {
+		        document.attachEvent('onreadytstatechange', function () {
+		              if (document.readyState == "complete") {
+		                        document.detachEvent("onreadystatechange", arguments.callee);
+		                        callback();
+		               }
+		        })
+		    }
+		    else if (document.lastChild == document.body) {
+		        callback();
+		    }
+		},
+		css:function(node,attr,value) {
+		    switch (arguments.length) {
+		        case 2:
+		            if (typeof arguments[1] == "object") { 
+		                for (var i in attr) node.style[i] = attr[i]
+		            }
+		            else { 
+		                return node.currentStyle ? node.currentStyle[attr] : getComputedStyle(node, null)[attr]
+		            }
+		            break;
+		        case 3:
+		            node.style[attr] = value;
+		            break;
+		        default:
+		            return "";
+		    }
+		},
+		hasClass:function(node,name){
+		   return  new RegExp('\\b'+name+'\\b','g').test(node.className);
+		},
+		addClass:function(obj,cls){
+		   if(!this.hasClass(obj,cls)){
+		   	if(obj.className==''){
+		   		obj.className=cls;
+		   	}else{
+		   		obj.className+=' '+cls;
+		   	}
+		   }
+		},
+		removeClass:function(node,name){
+		    if(this.hasClass(node,name)){
+		    	var lastClass = new RegExp('\\s'+name+'\$');
+		    	var classname = node.className;
+
+		    	if(lastClass.test(node.className)){
+		    		 node.className = classname.replace(lastClass,'');
+		    	}else{
+		    		 node.className = classname.replace(new RegExp('\\b'+name+'\\s\*\\b','g'),'');
+		    	}
+		    }
+		},
+		on:function(node,type,fn){
+			node.lib=node.lib||{};
+			node.lib[type]=node.lib[type]||[];
+			node.lib[type].push(fn)
+			if(node.addEventListener){
+				node.addEventListener(type,fn,false)
+			}else{
+				var tuo=function(){
+					fn.call(node)
+				}
+				node.lib[type].push(tuo)
+				node.attachEvent('on'+type,tuo);
+			}
+		},
+		pao:function(e,type,fn){
+			var ev=e||window.event;
+			var tag=ev.target||ev.srcElement;
+			if(tag.tagName.toLowerCase()==type){
+				fn();
+			}
+		},
+		off:function(node,type){
+			if(node.removeEventListener){
+				for(var i=0;i<node.lib[type].length;i++){
+					node.removeEventListener(type,node.lib[type][i],false);
+				}
+				node.lib[type]=[];
+			}else{
+				for(var i=0;i<node.lib[type].length;i++){
+					node.detachEvent('on'+type,node.lib[type][i]);
+				}
+				node.lib[type]=[];
+				
+			}
+		},
+		firstChild:function(node){
+			return node.children[0] ? node.children[0] : false;
+		},
+		lastChild:function(node){
+			var ls=node.children.length-1;
+			return node.children[ls] ? node.children[ls] : false;
+		},
+		next:function(node){
+			return node.nextElementSibling ? node.nextElementSibling : node.nextSibling;
+		},
+		prev:function(node){
+			return node.previousElementSibling ? node.previousElementSibling : node.previousSibling;
+		},
+		getPxToBody:function (node,type){
+			var a=node.offsetParent;
+			var b=node['offset'+type];
+			while(a){
+				b+=a['offset'+type];
+				a=a.offsetParent;
+			}
+			return b;
+		},
+		getPxToParent:function(node,type){
+			return this.getPxToBody(node,type)-this.getPxToBody(node.parentNode,type)
 		}
-		
 	}
+	window.q=q;
 })(window)
