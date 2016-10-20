22c308d6e8f6d4b06e908628c8dec0f9d0a545c9
millent <millent@126.com>
2016-10-20 11:58:28 +0800

    Update phone.js

--- phone/phone.js
+++ phone/phone.js
@@ -1,38 +1,76 @@
 (function(root){
+	function FactoryBoom(){
+		var data = {};
+		this.index = 1;
+		this.createFactory=function(){
+			var id = this.createId();
+			data[id] = {};
+			return id;
+		};
+		this.get = function(){
+			return data;
+		};
+		this.addRoom = function(id,dataName){
+			data[id][dataName] = data[id][dataName] || {};
+			return {
+				add:function(name,datas){
+					return data[id][dataName][name] = data[id][dataName][name] || datas;
+				},
+				remove : function(name){
+					delete data[id][dataName][name];
+				}
+			};
+		};
+		this.removeRoom = function(id,dataName){
+			delete data[id][dataName];
+		};
+	};
+	FactoryBoom.prototype.createId=function(){
+		return this.index++;
+	};
+	var fnFactory = new FactoryBoom();
+
+
+
+
 	function Factory(node){
 		this.node=node;
 	};
 	Factory.prototype.mousedown=function(fn){
+		var root = this;
 		this.on('touchstart',function(event){
 			if(event.targetTouches.length == 1){
-				fn(event.targetTouches[0]);
+				fn.call(root.node,event.targetTouches[0]);
 			}
 		})
 	};
 	Factory.prototype.mouseup=function(fn){
+		var root = this;
 		this.on('touchend',function(event){
-			fn(event.targetTouches[0]);
+			fn.call(root.node,event.targetTouches[0]);
 		})
 	};
 	Factory.prototype.mousemove=function(fn){
+		var root = this;
 		this.on('touchmove',function(event){
 			if(event.targetTouches.length == 1){
-				
-				fn(event.targetTouches[0]);
+				fn.call(root.node,event.targetTouches[0]);
 			}
 		})
 	};
 	Factory.prototype.scroll=function(fn,fn2){
 		var root=this;
+		var first=0;
 		this.mousedown(function(e){
 			root.on('scroll',function(e){
-				fn(e);
+				first=document.body.scrollTop || document.documentElement.scrollTop;
+				fn(e,first);
 			})
 			if(fn2){
-				var first=document.body.scrollTop || document.documentElement.scrollTop;
+				var foog = document.body.scrollTop || document.documentElement.scrollTop;
 				root.mouseup(function(e){
 					
-					var last=first-document.body.scrollTop || document.documentElement.scrollTop;
+					var last=foog-document.body.scrollTop || document.documentElement.scrollTop;
 					if(last>0){
 						fn2(e,1);
 					}else if(last==0){
@@ -40,30 +78,27 @@
 					}else{
 						fn2(e,-1);
 					}
-					
 					root.off(root.node,'touchend');
 					root.off(root.node,'scroll')
-
 				})	
 			}
 			
 		})
 	};
 	Factory.prototype.on=function(type,fn){
-		this.node.lib=this.node.lib||{};
-		this.node.lib[type]=this.node.lib[type]||[];
-		var g=function(event){
-			fn.call(this,event)
-		}
-		this.node.lib[type].push(g)
-
-		this.node.addEventListener(type,g,false)
+		this.node.fId = this.node.fId || fnFactory.createFactory();
+		var fner = fnFactory.addRoom(this.node.fId,'fn');
+		var typeArr = fner.add(type,[]);
+		typeArr.push(fn);
+		this.node.addEventListener(type,fn,false)
 	};
 	Factory.prototype.off=function(node,type){
-		for(var i=0;i<node.lib[type].length;i++){
-			node.removeEventListener(type,node.lib[type][i],false);
+		var fner = fnFactory.addRoom(node.fId,'fn');
+		var typeArr = fner.add(type,[]);
+		for(var i=0;i<typeArr.length;i++){
+			node.removeEventListener(type,typeArr[i],false);
 		}
-		node.lib[type]=[];
+		fner.remove(type);
 	};
 	Factory.prototype.css=function(attr,value) {
 	    switch (arguments.length) {
